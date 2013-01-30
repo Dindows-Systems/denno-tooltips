afb3099d5e8cac2a4dea43528e3867ae618ae7ea
dineshkummarc <dineshkummarc@gmail.com>
2013-01-30 12:00:59 +0530

    changed structure
    changed structure

---
+++ tipuedrop/tipuedrop/tipuedrop_content.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "Tipue drop", "thumb": "img/drop.png", "text": "A jQuery search suggestion box", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
+     {"title": "Tipue Search", "thumb": "img/search.png", "text": "A jQuery site search engine", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"},
+     {"title": "About Tipue", "thumb": "img/about.png", "text": "Tipue is a small web development studio based in North London", "tags": "", "loc": "http://www.tipue.com/about"}
+]};
